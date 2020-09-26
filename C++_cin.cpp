d17375043e8cf7f84973fb47d8c25475fc1345eb
HotMonkey <2608248307@qq.com>
2020-09-26 17:35:00 +0800

    update

---
+++ C++_cin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int sum;
+    int value;
+    while (cin>>value)
+    {
+        sum+=value;//2
+        /* code */
+    }
+    cout<<sum<<endl;
+}
---
+++ C++_type.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){    
+    char day ='0';
+    double num = 1.23e-3;
+    cout<<day<<endl;
+    cout<<num<<endl;
+    cout<<"00"<<num<<"222"<<endl;
+    cout<<"/*";
+    cout<<"*/";
+    //cout<</*"*/"*/;
+    return 0;
+}
